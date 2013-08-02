2c6e7f426c78bb097947e992f0dfcaeebdee0e77
shenjc <shenjiechuan@yeah.net>
2013-08-02 14:20:32 +0800

    新加

---
+++ Mzpj_farypzService.java
@@ -0,0 +1,19 @@
+package cn.com.hnisi.jxkh.service;
+
+import java.sql.Connection;
+
+/**
+ * 2013年8月2日
+ * @author shenjiechuan
+ *民主评价-方案人员配置
+ */
+public class Mzpj_farypzService
+{
+    private boolean flag=false;
+    
+    public boolean isUpdated(String SQL) {
+//        Connection connection=getConnection();
+        return flag;
+    }
+
+}
